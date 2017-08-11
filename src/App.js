2557f7cf6471b06ce8b8e8de49726027d198d83a
Ariel Yanarico <argos.yo@gmail.com>
2017-08-11 12:00:06 -0400

    feat: Add Book component
    I added Book component and added semicolons to expressions, I also added propTypes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react'
-import SearchBooks from './SearchBooks'
-import ListBooks from './ListBooks'
-import { Route } from 'react-router-dom'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
+import React, {Component} from 'react';
+import SearchBooks from './SearchBooks';
+import ListBooks from './ListBooks';
+import { Route } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import './App.css';
 
 class BooksApp extends Component {
   state = {
@@ -18,23 +18,24 @@ class BooksApp extends Component {
 
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
-      this.setState({ books })
-      console.log(JSON.stringify(books));
-    })
+      this.setState({ books });
+    });
   }
 
   render() {
+    const { books } = this.state
+
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <ListBooks/>
+          <ListBooks books={ books }/>
         )}/>
         <Route path="/search" render={() => (
           <SearchBooks/>
         )}/>
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
